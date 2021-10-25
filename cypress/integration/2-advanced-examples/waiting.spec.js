3a0b8d12dd7e62bec6dd0a73b60904eafd23ef48
Walmyr Filho <limaesilva@qelp.com>
2021-10-26 00:13:16 +0200

    Slow a test on purpose
    And add a npm script to run it.
    
    This way I can show what happens when a test takes longer than the
    default `slowTestThreshold` property, which is of 10000ms for e2e
    tests.
    
    Please refer to:
    https://docs.cypress.io/guides/references/changelog#8-7-0

--- cypress/integration/2-advanced-examples/waiting.spec.js
+++ cypress/integration/2-advanced-examples/waiting.spec.js
@@ -11,8 +11,8 @@ context('Waiting', () => {
   it('cy.wait() - wait for a specific amount of time', () => {
     cy.get('.wait-input1').type('Wait 1000ms after typing')
     cy.wait(1000)
-    cy.get('.wait-input2').type('Wait 1000ms after typing')
-    cy.wait(1000)
+    cy.get('.wait-input2').type('Wait 10000ms after typing')
+    cy.wait(10000)
     cy.get('.wait-input3').type('Wait 1000ms after typing')
     cy.wait(1000)
   })
